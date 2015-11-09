fb2299c37dafa4769fa8f0fa345f3f1779452a67
Keenaghan <LiamKeenaghan5@Gmail.com>
2015-11-09 11:28:02 +0000

    Update a1-server.js

--- a1-server.js
+++ a1-server.js
@@ -47,7 +47,7 @@ server.listen(server_port, server_ip_address, function(){
     
     // ASSIGNMENT
     // 3. Add a debug message which ouputs a message indicating the server is started (listening for user requests).
-    console.re.log("HTTP Server listening on:");
+    console.re.log("HTTP Server is listening");
     // 4. Add a debug message which ouputs your server ip address and your server port number. Your server ip address 
     // and your server port number values are available in the server_ip_address and server_port program global variables
     console.re.log("HTTP Server listening on: " + server_ip_address + ":" + server_port);
